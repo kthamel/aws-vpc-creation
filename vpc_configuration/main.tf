8d23fa39713053465222cb7d8b185ebda51555da
Kushan Thamel <kushan.thamel@gmail.com>
2024-03-12 23:21:32 +0000

    fetching the values from vault rev-13

--- vpc_configuration/main.tf
+++ vpc_configuration/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_vpc" "kthamel-ec2-vpc" {
   cidr_block                       = "172.32.0.0/16"
   assign_generated_ipv6_cidr_block = false
