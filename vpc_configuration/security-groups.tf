4bf9856393b77714d1171e9b15767f25da697525
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-12 20:21:33 +0100

    modified the firewall rules

--- vpc_configuration/security-groups.tf
+++ vpc_configuration/security-groups.tf
@@ -1,9 +1,9 @@
 resource "aws_security_group" "public-subnet-assoc" {
   name = "public-subnet-sg"
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
