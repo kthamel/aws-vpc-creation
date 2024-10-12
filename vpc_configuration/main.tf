6424f7e0300cfa2c6f211014d7fc9cc6674526d1
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-12 12:21:44 +0100

    modified the subnet configuration

--- vpc_configuration/main.tf
+++ vpc_configuration/main.tf
@@ -24,6 +24,7 @@ resource "aws_subnet" "kthamel-ec2-subnet-1" {
   vpc_id            = aws_vpc.kthamel-ec2-vpc.id
   cidr_block        = "172.32.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = local.common_tags
 }
@@ -32,7 +33,6 @@ resource "aws_subnet" "kthamel-ec2-subnet-2" {
   vpc_id                  = aws_vpc.kthamel-ec2-vpc.id
   cidr_block              = "172.32.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
 
   tags = local.common_tags
 }
