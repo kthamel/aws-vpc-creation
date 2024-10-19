69284ff9122c5f52ecb6d0b68b8a47d8695333bd
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-19 18:41:03 +0100

    adding more subnets rev-03

--- vpc_configuration/main.tf
+++ vpc_configuration/main.tf
@@ -29,35 +29,19 @@ resource "aws_subnet" "kthamel-ec2-subnet-1" {
   tags = local.common_tags
 }
 
-resource "aws_subnet" "kthamel-ec2-subnet-2" {
-  vpc_id            = aws_vpc.kthamel-ec2-vpc.id
-  cidr_block        = "172.32.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = local.common_tags
-}
-
-resource "aws_subnet" "kthamel-ec2-subnet-3" {
-  vpc_id            = aws_vpc.kthamel-ec2-vpc.id
-  cidr_block        = "172.32.3.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = local.common_tags
-}
-
-resource "aws_subnet" "kthamel-ec2-subnet-4" {
+resource "aws_subnet" "kthamel-ec2-subnet-0" {
   vpc_id                  = aws_vpc.kthamel-ec2-vpc.id
-  cidr_block              = "172.32.4.0/24"
-  availability_zone       = "us-east-2a"
+  cidr_block              = "172.32.2.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = local.common_tags
 }
 
-resource "aws_subnet" "kthamel-ec2-subnet-5" {
+resource "aws_subnet" "kthamel-ec2-subnet-1" {
   vpc_id                  = aws_vpc.kthamel-ec2-vpc.id
-  cidr_block              = "172.32.5.0/24"
-  availability_zone       = "us-east-2b"
+  cidr_block              = "172.32.3.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = local.common_tags
