25c72426760f754a22a328514a277f506ee55f3b
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-19 18:28:25 +0100

    more subnets were added to alb

--- vpc_configuration/alb.tf
+++ vpc_configuration/alb.tf
@@ -1,10 +1,10 @@
 resource "aws_lb" "kthamel-eks-alb" {
-  name               = "kthamel-eks-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["sg-06ef98d8997c35818"]
+  name                       = "kthamel-eks-alb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = ["sg-06ef98d8997c35818"]
   enable_deletion_protection = false
-
+  subnets                    = ["subnet-025d011a84e801ae7", "subnet-0dedaaf475b652bdc"]
   access_logs {
     bucket  = "kthamel-alb-access-logs"
     prefix  = "aws-eks"
