3aebda0da7d7189f8f8f76ce18970128962931f8
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-19 18:31:14 +0100

    adding more subnets

--- vpc_configuration/main.tf
+++ vpc_configuration/main.tf
@@ -44,3 +44,21 @@ resource "aws_subnet" "kthamel-ec2-subnet-3" {
 
   tags = local.common_tags
 }
+
+resource "aws_subnet" "kthamel-ec2-subnet-4" {
+  vpc_id                  = aws_vpc.kthamel-ec2-vpc.id
+  cidr_block              = "172.32.4.0/24"
+  availability_zone       = "us-east-ba"
+  map_public_ip_on_launch = true
+
+  tags = local.common_tags
+}
+
+resource "aws_subnet" "kthamel-ec2-subnet-5" {
+  vpc_id                  = aws_vpc.kthamel-ec2-vpc.id
+  cidr_block              = "172.32.5.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = local.common_tags
+}
