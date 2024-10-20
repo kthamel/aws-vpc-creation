c69f69aac6ca95be3362d32ce27a2778b9ceea25
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-20 21:08:34 +0100

    reconfigured the nat gw

--- vpc_configuration/nat-gw.tf
+++ vpc_configuration/nat-gw.tf
@@ -6,7 +6,7 @@ resource "aws_eip" "kthamel-ec2-elastic-ip" {
 
 resource "aws_nat_gateway" "kthamel-ec2-nat-gw" {
   allocation_id = aws_eip.kthamel-ec2-elastic-ip.id
-  subnet_id     = aws_subnet.kthamel-ec2-subnet-pub-core.id
+  subnet_id     = aws_subnet.kthamel-ec2-subnet-pub-core-1a.id
 
   depends_on = [aws_internet_gateway.kthamel-ec2-igw]
 
