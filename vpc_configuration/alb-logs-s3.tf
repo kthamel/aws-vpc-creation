0f20012e4b1f21906a1848e6e4dd193a9ea0b775
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-21 06:09:37 +0100

    update bucket policy

--- vpc_configuration/alb-logs-s3.tf
+++ vpc_configuration/alb-logs-s3.tf
@@ -1,51 +1,59 @@
-resource "aws_s3_bucket" "kthamel-alb-access-logs" {
-  bucket = "kthamel-s3-alb-access-logs"
+# Step 1: Define the S3 bucket
+resource "aws_s3_bucket" "alb_access_logs" {
+  bucket = "kthamel-alb-access-logs"
+
+  versioning {
+    enabled = true
+  }
+
+  acl = "private"
 
   tags = local.common_tags
 }
 
-resource "aws_s3_bucket_policy" "allow_alb_logging" {
-  bucket = aws_s3_bucket.kthamel-alb-access-logs.id
+# Step 2: Define the bucket policy
+resource "aws_s3_bucket_policy" "alb_access_logs_policy" {
+  bucket = aws_s3_bucket.alb_access_logs.id
+
   policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
-      },
-      "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::kthamel-alb-access-logs/alb-logs/*",
-      "Condition": {
-        "StringEquals": {
-          "s3:x-amz-acl": "bucket-owner-full-control"
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        },
+        Action = "s3:PutObject",
+        Resource = "arn:aws:s3:::kthamel-alb-access-logs/alb-logs/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
         }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
       },
-      "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::kthamel-alb-access-logs/*",
-      "Condition": {
-        "StringEquals": {
-          "s3:x-amz-acl": "bucket-owner-full-control"
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        },
+        Action = "s3:PutObject",
+        Resource = "arn:aws:s3:::kthamel-alb-access-logs/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
         }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Principal": {
-        "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
       },
-      "Resource": "arn:aws:s3:::kthamel-alb-access-logs"
-    }
-  ]
-}
-)
+      {
+        Effect = "Allow",
+        Action = "s3:ListBucket",
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        },
+        Resource = "arn:aws:s3:::kthamel-alb-access-logs"
+      }
+    ]
+  })
 }
 
-data "aws_caller_identity" "current" {}
+
