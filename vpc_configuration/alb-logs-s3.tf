e55999c0045b193b60724939d2944acde4ce4414
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-23 14:29:38 +0100

    modified bucket policy re-04

--- vpc_configuration/alb-logs-s3.tf
+++ vpc_configuration/alb-logs-s3.tf
@@ -15,16 +15,16 @@ resource "aws_s3_bucket" "kthamel-alb-access-logs" {
 resource "aws_s3_bucket_policy" "kthamel-alb-access-logs-bucket-policy" {
   bucket = aws_s3_bucket.kthamel-alb-access-logs.id
   policy = jsonencode({
-  "Id": "Policy1729689145524",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1729689143218",
-      "Action": "s3:*",
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::kthamel-alb-access-logs/alb-logs",
-      "Principal": "*"
-    }
-  ]
-})
+    "Id" : "Policy1729689145524",
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Stmt1729689143218",
+        "Action" : "s3:*",
+        "Effect" : "Allow",
+        "Resource" : "arn:aws:s3:::kthamel-alb-access-logs/alb-logs",
+        "Principal" : "*"
+      }
+    ]
+  })
 }
