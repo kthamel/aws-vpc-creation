a9cf018fa5acfbba43c0570416db77e184edee49
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-23 14:27:56 +0100

    modified bucket policy re-02

--- vpc_configuration/alb-logs-s3.tf
+++ vpc_configuration/alb-logs-s3.tf
@@ -23,11 +23,7 @@ resource "aws_s3_bucket_policy" "kthamel-alb-access-logs-bucket-policy" {
       "Action": "s3:*",
       "Effect": "Allow",
       "Resource": "arn:aws:s3:::kthamel-alb-access-logs/alb-logs",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::us-east-1"
-        ]
-      }
+      "Principal": {"*"}
     }
   ]
 })
